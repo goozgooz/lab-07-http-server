88615a6bed85ce7a7724b7ed0b9c7eb1d941afa8
Ashkaan Jaberi <ashkaan14@gmail.com>
2017-10-11 20:23:49 -0700

    lab done

--- test/server-test.js
+++ test/server-test.js
@@ -19,7 +19,7 @@ describe('my http server', function() {
     server.close(done);
   });
 
-  it('should return a cow saying "Welcome To Server" to a get request', function(done){
+  it('should return a cow saying "Welcome To Server" to a GET request at /', function(done){
     request
       .get(host + '/')
       .end((err,res) => {
@@ -28,4 +28,36 @@ describe('my http server', function() {
         done();
       });
   });
+  
+  it('should return a cow saying the query parameter given to a GET request', function(done){
+    request
+      .get(host + '/cowsay?text=moo')
+      .end((err,res) => {
+        expect(err).toBe(null);
+        expect(res.text).toBe(cowsay.say({text: 'moo'}));
+        done();
+      });
+  });
+  
+  it('should return a cow saying the JSON sent in a POST request', function(done){
+    request
+      .post(host + '/')
+      .send({test: 'cool beans'})
+      .end((err,res) => {
+        expect(err).toBe(null);
+        expect(res.text).toBe(cowsay.say({text: '{"test":"cool beans"}'}));
+        done();
+      });
+  });
+  
+  it('should return an error if bad JSON', function(done){
+    request
+      .post(host + '/')
+      .send('asdfa')
+      .end((err,res) => {
+        expect(err).not.toBe(null);
+        done();
+      });
+  });
+
 });
